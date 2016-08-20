430d5b61c564f22acb98538c90d0044da620fc64
Byron Dover <byrondover+github.com@gmail.com>
2016-08-20 02:11:17 -0700

    Add PART GROUP column

--- excelerator.py
+++ excelerator.py
@@ -5,39 +5,72 @@ from collections import OrderedDict
 from openpyxl import load_workbook
 from openpyxl.styles import Font
 
+# Set global variables for later use.
+bold = Font(bold=True)
+
+
 def append_data(sheet, data):
     sorted_data = sorted(data, key=lambda k: k['PART NUMBER'])
 
     sheet.append(sorted_data[0].keys())
 
-    for row in data:
+    for row in sorted_data:
         sheet.append(row.values())
+
+
+def apply_styles(workbook):
+    for i in range(1, len(workbook.worksheets)):
+        dims = {}
+        ws = workbook.worksheets[i]
+
+        for cell in ws.rows[0]:
+            cell.font = bold
+
+        for j, row in enumerate(ws.rows):
+            padding = 2 if j == 0 else 1
+
+            for cell in row:
+                if cell.value:
+                    dims[cell.column] = max((dims.get(cell.column, 0),
+                        len(str(cell.value)) + padding, 4))
+
+        for col, value in dims.items():
+            ws.column_dimensions[col].width = value
+
 
 def create_parts_list(sheet, initial_row = 0):
     first_row_number = find_first_row(sheet, initial_row)
-    first_row = 'A' + str(first_row_number)
-
     last_row_number = find_last_row(sheet, first_row_number)
-    last_row = 'Z' + str(last_row_number)
+
+    first_row = 'A' + str(first_row_number + 1)
+    last_row = 'Z' + str(last_row_number + 1)
 
     parts_list = list(sheet.iter_rows(first_row + ':' + last_row))
 
     return parts_list, last_row_number
 
-def create_section_list(source_cells, section):
+
+def create_section_list(source_cells, section, part_group):
     headers = [i.value for i in source_cells[0] if i.value != None]
 
     def item(i, j):
-        return (source_cells[0][j].value, section[i][j].value)
+        if j == len(headers):
+            attr = ('PART GROUP', part_group)
+        else:
+            attr = (source_cells[0][j].value, section[i][j].value)
 
-    return [OrderedDict(item(i, j) for j in range(len(headers)))
+        return attr
+
+    return [OrderedDict(item(i, j) for j in range(len(headers) + 1))
         for i in range(1, len(section))]
+
 
 def create_sheet(workbook, name):
     sheet = workbook.create_sheet()
     sheet.title = name
 
     return sheet
+
 
 def find_first_row(source_sheet, row = 0):
     cell_value = str()
@@ -46,121 +79,74 @@ def find_first_row(source_sheet, row = 0):
         cell_value = source_sheet.rows[row][0].value
         row += 1
 
-    return row
+    return row - 1
+
 
 def find_last_row(source_sheet, row):
     cell_value = str()
-    print row
 
     while cell_value != None:
         cell_value = source_sheet.rows[row][0].value
         row += 1
 
-    return row - 1
+    return row - 2
 
 def excelerate(file):
     # Load spreadsheet into Workbook object.
     wb = load_workbook(file)
 
-    # Set styles variables for later use.
-    bold = Font(bold=True)
-
-    weld_picklist_sheet = wb.create_sheet()
-    weld_picklist_sheet.title = 'WELD SCF Picklist'
-
+    # First spreadsheet should contain master parts list.
     parts_list_sheet = wb.active
 
-    start_row_number = find_first_row(parts_list_sheet)
-    start_row = 'A' + str(start_row_number)
-    end_row_number = find_last_row(parts_list_sheet, start_row_number)
-    end_row = 'Z' + str(end_row_number)
-
-    #fabricated_parts = parts_list[start_row:end_row]
-    fabricated_parts = list(parts_list_sheet.iter_rows(start_row + ':' + end_row))
-
-    start_row_number = find_first_row(parts_list_sheet, end_row_number + 1)
-    start_row = 'A' + str(start_row_number)
-    end_row_number = find_last_row(parts_list_sheet, start_row_number + 1)
-    end_row = 'Z' + str(end_row_number)
-
-    #weldments = parts_list[start_row:end_row]
-    weldments = list(parts_list_sheet.iter_rows(start_row + ':' + end_row))
-
-    start_row_number = find_first_row(parts_list_sheet, end_row_number + 1)
-    start_row = 'A' + str(start_row_number)
-    end_row_number = find_last_row(parts_list_sheet, start_row_number + 1)
-    end_row = 'Z' + str(end_row_number)
-
-    #purchased_parts = parts_list[start_row:end_row]
-    #purchased_parts = list(parts_list_sheet.iter_rows(start_row + ':' + end_row))
+    # Iterate through master parts list and identify each section.
     fabricated_parts, last_row_number = create_parts_list(parts_list_sheet)
-
     weldments, last_row_number = create_parts_list(
         parts_list_sheet, last_row_number + 1)
-
     purchased_parts, last_row_number = create_parts_list(
         parts_list_sheet, last_row_number + 1)
 
-    fabricated_list = create_section_list(fabricated_parts, fabricated_parts)
-    weldments_list = create_section_list(fabricated_parts, weldments)
-    purchased_list = create_section_list(fabricated_parts, purchased_parts)
+    # Create lists of dictionarys for each section.
+    fabricated_list = create_section_list(
+        fabricated_parts, fabricated_parts, 'FabricatedParts')
+    weldments_list = create_section_list(
+        fabricated_parts, weldments, 'WeldmentParts')
+    purchased_list = create_section_list(
+        fabricated_parts, purchased_parts, 'PurchasedFabParts')
 
     full_list = fabricated_list + weldments_list + purchased_list
 
-    """
-    for i, header in enumerate(fabricated_list):
-        if str(header) == 'WELDED':
-            welded_idx = i
-        if str(header) == 'WELDMENT USED':
-            weldment_used_idx = i
-    """
-
+    # Create Weld SFC Pick List sheet.
+    weld_picklist_sheet = create_sheet(wb, 'WELD SCF Pick List')
     append_data(weld_picklist_sheet, fabricated_list)
 
+    # Create WELD BOM sheet.
     weld_bom_sheet = create_sheet(wb, 'WELD BOM')
-
     weld_bom_data = [x for x in full_list if str(x['WELDED']) == 'WELDED' and
         str(x['WELDMENT USED']) != 'SHIPPED LOOSE']
-
     append_data(weld_bom_sheet, weld_bom_data)
 
+    # Create WELD LOOSE sheet.
     weld_loose_sheet = create_sheet(wb, 'WELD LOOSE')
-
     weld_loose_data = [x for x in full_list if str(x['WELDED']) == 'WELDED' and
         str(x['WELDMENT USED']) == 'SHIPPED LOOSE']
-
     append_data(weld_loose_sheet, weld_loose_data)
 
+    # Create WELD Packing Slip sheet.
     weld_packing_sheet = create_sheet(wb, 'WELD Packing Slip')
-
     weld_packing_data = [x for x in full_list if str(x['WELDMENT USED']) ==
             'SHIPPED LOOSE']
-
     append_data(weld_packing_sheet, weld_packing_data)
 
+    # Create FINISH Pick List sheet.
     finish_picklist_sheet = create_sheet(wb, 'FINISH Pick List')
-
     finish_picklist_data = [x for x in full_list if str(x['WELDMENT USED']) ==
             'SHIPPED LOOSE']
-
     append_data(finish_picklist_sheet, finish_picklist_data)
 
     # Apply styles.
-    for i in range(1, len(wb.worksheets)):
-        dims = {}
-        ws = wb.worksheets[i]
-
-        for cell in ws.rows[0]:
-            cell.font = bold
-
-        for row in ws.rows:
-            for cell in row:
-                if cell.value:
-                    dims[cell.column] = max((dims.get(cell.column, 0),
-                        len(str(cell.value)), 4))
-        for col, value in dims.items():
-            ws.column_dimensions[col].width = value
+    apply_styles(wb)
 
     wb.save("test_complete.xlsx")
+
 
 excelerate('test.xlsx')
