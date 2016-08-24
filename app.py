159afc1d8852bd8479f5940b51ae5c0df018de15
Byron Dover <byrondover+github.com@gmail.com>
2016-08-24 01:01:22 -0700

    Add initial Google App Engine support

--- app.py
+++ app.py
@@ -45,5 +45,15 @@ def get_tasks():
     )
 
 
+@app.route('/error')
+@app.errorhandler(500)
+def server_error(e=str()):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
